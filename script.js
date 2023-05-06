6f9e0d85a8aa24f684ee09d1a8f1cb3d13e1954b
jenzie <jacecabalo@gmail.com>
2023-05-06 14:34:26 +0400

    add function displaybook

--- script.js
+++ script.js
@@ -13,12 +13,36 @@ Book.prototype.info = function () {
 
 function addBookToLibrary() {
   const userInput = new Book(
-    prompt(`type title of book`),
-    prompt(`type author of book`),
-    prompt("how many pages does this book have?"),
-    prompt("Have you read it or not")
-  );
+    prompt(`type title of book`, "Atomic Habits"),
+    prompt(`type author of book`, "James Clear"),
+    prompt("how many pages does this book have?", "298 pages"),
+    prompt("Have you read it or not", "Yes")
+ );
+
   myLibrary.push(userInput);
 }
 
+function displayBook() {
+  const container = document.querySelector(".container");
+
+  myLibrary.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    const titleBook = document.createElement('p');
+    const authorBook = document.createElement('p');
+    const pagesBook = document.createElement('p');
+    const readBook = document.createElement('p');
+    authorBook.textContent = book.author;
+    titleBook.textContent = book.title;
+    pagesBook.textContent = book.pages;
+    readBook.textContent = book.read;
+    div.appendChild(titleBook);
+    div.appendChild(authorBook);
+    div.appendChild(pagesBook);
+    div.appendChild(readBook);
+    container.appendChild(div);
+  });
+}
+
 addBookToLibrary();
+displayBook()
