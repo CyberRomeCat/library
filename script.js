d818ae5f9040e54555aaa957551bac210ee25d4c
jenzie <jacecabalo@gmail.com>
2023-05-06 13:26:34 +0400

    add constructor function book objects,add function to add books in array

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let myLibrary = [];
 
-function Book(title,author,pages,read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-Book.prototype.info = function() {
-    console.log(`${this.title} by ${this.author},${this.pages},${this.read}`)
+Book.prototype.info = function () {
+  console.log(`${this.title} by ${this.author},${this.pages},${this.read}`);
 };
 
 function addBookToLibrary() {
-    const userInput = prompt('Which book would you like to add','TOP book');
-};
+  const userInput = new Book(
+    prompt(`type title of book`),
+    prompt(`type author of book`),
+    prompt("how many pages does this book have?"),
+    prompt("Have you read it or not")
+  );
+  myLibrary.push(userInput);
+}
+
+addBookToLibrary();
