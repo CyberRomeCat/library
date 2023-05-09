9a299085f99b2ed4e2f29077f6d2b1b05910e1a6
jenzie <jacecabalo@gmail.com>
2023-05-09 17:56:11 +0400

    add exit button form

--- script.js
+++ script.js
@@ -1,89 +1,87 @@
 let myLibrary = [];
 
-let lastElement = myLibrary.splice(-2,1);
-
-function Book(title, author, pages, read) {
+function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.status = status;
 }
 
 Book.prototype.info = function () {
-  console.log(`${this.title} by ${this.author},${this.pages},${this.read}`);
+  console.log(`${this.title} by ${this.author},${this.pages},${this.status}`);
 };
 
 function addBookToLibrary() {
+  let bookStatusValue = 'Not Done Reading';
+
   const bookName = document.getElementById("book-name");
   const bookAuthor = document.getElementById("author");
   const bookPages = document.getElementById("pages");
-  const bookRead = document.getElementById("read-notRead");
-
-  let bookReadValue = "";
+  const bookStatus = document.getElementById("read-notRead");
+  const popUpForm = document.getElementById("formPopUp");
 
   if ((bookName.value || bookAuthor.value || bookPages.value) == "") {
     console.log("error pls type again");
   } else {
-    if (bookRead.checked) {
-      bookReadValue = "Done reading";
-    } else {
-      bookReadValue = "Not Done reading";
-    }
-    const userInput = new Book(
+    if (bookStatus.checked) {
+      bookStatusValue = 'Done reading';
+    }; 
+    let userInput = new Book(
       bookName.value,
       bookAuthor.value,
       bookPages.value,
-      bookReadValue
+      bookStatusValue
     );
     console.log("your book has been pushed :)");
     myLibrary.push(userInput);
     displayBook();
-    bookName.value = '';
-    bookAuthor.value = '';
-    bookPages.value = '';
-  }
-}
+    bookName.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    popUpForm.style.display = 'none'
+  };
+};
 
 function displayBook() {
   const container = document.querySelector(".container");
 
   myLibrary.forEach((book) => {
     const div = document.createElement("div");
-    const btnRemove = document.createElement('button');
-    const readStatus = document.createElement('input');
-    readStatus.setAttribute('checkbox')
+    const btnRemove = document.createElement("button");
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("id", "read-notRead");
     div.classList.add("card");
     const titleBook = document.createElement("p");
     const authorBook = document.createElement("p");
     const pagesBook = document.createElement("p");
     const readBook = document.createElement("p");
-    btnRemove.textContent = 'DELETE';
+    btnRemove.textContent = "DELETE";
     authorBook.textContent = book.author;
     titleBook.textContent = book.title;
     pagesBook.textContent = book.pages;
     readBook.textContent = book.read;
-    div.appendChild(readStatus);
+    div.appendChild(checkBox);
     div.appendChild(titleBook);
     div.appendChild(authorBook);
     div.appendChild(pagesBook);
     div.appendChild(readBook);
     div.appendChild(btnRemove);
     container.appendChild(div);
-    
 
-    btnRemove.addEventListener('click',() => {
+    btnRemove.addEventListener("click", () => {
       div.remove();
       let target = book;
       let targetIndex = myLibrary.indexOf(target);
-      myLibrary.splice(targetIndex,1);
+      myLibrary.splice(targetIndex, 1);
     });
   });
-}
+};
 
 const addBookBtn = document.getElementById("addBook");
 addBookBtn.addEventListener("click", () => {
   const popUpForm = document.getElementById("formPopUp");
-  popUpForm.style.display = "block";
+  popUpForm.style.display = "inline-flex";
 });
 
 const bookForm = document.getElementById("book-form");
@@ -91,5 +89,12 @@ bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
   addBookToLibrary();
 });
+
+const hideForm = document.getElementById('hideForm')
+hideForm.addEventListener('click',() => {
+  const popUpForm = document.getElementById("formPopUp");
+  popUpForm.style.display = 'none';
+})
+
 
 displayBook();
